b7fbb2a369319689faa87639b47adf956c91f289
jiaurrahamankhanjrk <56629249+jiaurrahamankhanjrk@users.noreply.github.com>
2024-01-18 23:41:24 +0530

    Add files via upload

---
+++ L4.js
@@ -0,0 +1,24 @@
+/*                  // querySelector("")
+let result = document.querySelectorAll(".h");
+console.log(result);                                    */
+
+/*                      // querySelectorAll("")
+let a = document.querySelectorAll("#a");
+console.log(a);                                         */
+
+/*                             // change innter text by querySelector
+let c = document.querySelector("#a").innerText =  `Changed by JS - document.querySelector("").innerText `;                                      */
+
+
+/*                              // change html tag by querySelector
+let d = document.querySelector("#l").innerHTML = `<h2>Changed by JS - document.querySelector("").innerHTML</h2>`                            */
+
+
+/*                            // change innter text by querySelectorAll
+let c = document.querySelectorAll(".h");  
+console.log(c);                                  
+for (let i = 0; i < c.length; i++) {
+    let z = c[i].innerHTML = `changed`;
+    console.log(z);
+    
+}                                                                   */
